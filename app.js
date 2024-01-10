a666f5b6ffb336501a80276cc136a8c1f8d4ae57
Houcem Eddine Mabrouki <houcemeddine.mabrouki@etudiant-isi.utm.tn>
2024-01-10 20:32:46 +0100

    docker file

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors"); // Import the cors middleware
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 // Connexion à MongoDB
 mongoose.connect(
